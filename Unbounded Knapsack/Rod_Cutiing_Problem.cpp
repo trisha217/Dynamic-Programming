f32e6702f479ee0b6f73c62ea91f2bc37bf3279c
Trisha Moitra <trishamoitra.217@gmail.com>
2021-08-31 13:34:56 +0530

    Add files via upload

---
+++ Unbounded Knapsack/Rod_Cutiing_Problem.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+void Unbounded_Knapsack()
+{
+    int n;
+    cout << "Enter the length of the rod?" << endl;
+    cin >> n;
+    int wt[n];
+    int val[n];
+    cout << "Enter the length array:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> wt[i];
+    }
+    cout << "Enter the price array:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val[i];
+    }
+    int cap;
+    cout << "Enter the maximum length of the rod" << endl;
+    cin >> cap;
+    int dp[n + 1][cap + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= cap; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+            else if (wt[i - 1] <= j)
+            {
+                dp[i][j] = max(val[i - 1] + dp[i][j - wt[i - 1]], dp[i - 1][j]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    cout << "MAX PROFIT:" << dp[n][cap] << endl;
+}
+
+int main()
+{
+    Unbounded_Knapsack();
+    return 0;
+}
