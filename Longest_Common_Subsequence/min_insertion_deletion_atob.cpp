599dea1cebe68afeb8cf8e9614bf033942a9ae14
Trisha Moitra <trishamoitra.217@gmail.com>
2021-09-14 00:38:40 +0530

    Add files via upload

---
+++ Longest_Common_Subsequence/min_insertion_deletion_atob.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int lcs(string x, string y, int m, int n)
+{
+    int dp[m + 1][n + 1];
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (x[i - 1] == y[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+int main()
+{
+    string x, y;
+    int n, m;
+    cout << "Enter both the strings \n";
+    cin >> x >> y;
+    m = x.length();
+    n = y.length();
+    int res = lcs(x, y, m, n);
+    cout<<"Insertions:"<<(n-res)<<endl;
+    cout<<"Deletions:"<<(m-res)<<endl;
+    return 0;
+}
